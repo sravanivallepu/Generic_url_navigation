914eddb79729bd9ce611c4d101ebc1e57943e77c
SravaniVallepu <sravanivallepu00294@gmail.com>
2023-06-02 10:53:57 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def srujana(request):
+    return render(request,'srujana.html')
+
+def sai(request):
+    return render(request,'sai.html')
